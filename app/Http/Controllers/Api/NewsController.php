f0b136a59fd9502cdb50b825ab407caaabeae825
AshrafElshazly <ashrafshazli7@gmail.com>
2022-09-29 23:46:56 +0200

    Api test

---
+++ app/Http/Controllers/Api/NewsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Setting;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Traits\ApiResponseTrait;
+
+class NewsController extends Controller
+{
+    use ApiResponseTrait;
+
+    function index(){
+        $data['settings'] = Setting::first();
+        // return view('web.news.index',$data);
+        return $this->apiResponse(200,'test',null,$data);
+    }
+}
