d5af566bb556f8a549c9cdc486601f3b85e49e8a
AshrafElshazly <ashrafshazli7@gmail.com>
2022-10-07 14:32:36 +0200

    add repritory provider

--- config/app.php
+++ config/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Facade;
+use App\Providers\RepositoryServicesProvider;
 
 return [
 
@@ -196,6 +197,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
+        RepositoryServicesProvider::class,
 
     ],
 
