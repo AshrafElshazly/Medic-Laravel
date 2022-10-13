a96d80ec81444f2f64344e85eab8e8f1e8e4b2bf
AshrafElshazly <ashrafshazli7@gmail.com>
2022-10-13 20:04:42 +0200

    auth

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\{AdminHomeController,AuthController};
+use App\Http\Controllers\Admin\{AdminHomeController, AppointmentController as AdminAppointmentController, AuthController};
 use App\Http\Controllers\web\{HomeController,AboutController,ContactController,
     GalleryController,ServiceController,AppointmentController, FeedbackController, BookingController};
 
 
-
 //Admin Routes
 Route::prefix('admin')->name('admin.')->group(function () {
-
-    Route::middleware(['auth'])->group(function () {
-        Route::get('/',[AdminHomeController::class, 'index'])->name('dashboard');
-    });
-
+    
     Route::controller(AuthController::class)->group(function () {
         Route::get('/login','index')->name('loginPage');
         Route::post('/login','login')->name('login');
         Route::post('/logout','logout')->name('logout');    
     });
 
+    Route::middleware(['auth'])->group(function () {
+        Route::get('/',[AdminHomeController::class, 'index'])->name('dashboard');   
+        Route::get('/appointment',[AdminAppointmentController::class,'index'])->name('appointments');
+    }); 
 });
 
 
