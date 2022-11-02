a1adc1523d3a782496f18c0ac7c54ff479b0cc1a
AshrafElshazly <ashrafshazli7@gmail.com>
2022-11-02 21:57:10 +0200

    client model

---
+++ app/Models/Client.php
@@ -0,0 +1,53 @@
+<?php
+ 
+namespace App\Models;
+ 
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
+ 
+class Client extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'image',
+        'phone'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    // public static function rules(){
+    //     return [
+    //         'email' => 'required|string|email|max:255|exists:users',
+    //         'password' => 'required|string|min:6'
+    //     ];
+    // }
+
+}
